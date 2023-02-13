ffac27bffe6846c3a011b8739f55c82e362a1a76
ssesselmann <75355962+ssesselmann@users.noreply.github.com>
2023-02-14 09:03:53 +1100

    Fixed double browser opening

--- code/run.py
+++ code/run.py
@@ -14,11 +14,7 @@ log.setLevel(logging.ERROR)
 
 # Define default `8050` port
 port = 8050 
-
-# Function to open browser
-fn.open_browser(port)
-
-
+# Application must be started from (__main__)
 if __name__ == '__main__':
     Timer(1, fn.open_browser(port)).start();
     app.run_server(debug=False, port=port)
