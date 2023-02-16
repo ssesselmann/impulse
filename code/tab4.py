78c2d37d62b9d4b19e49b78558b043ef4941a515
ssesselmann <75355962+ssesselmann@users.noreply.github.com>
2023-02-16 15:47:42 +1100

    Update tab4.py

--- code/tab4.py
+++ code/tab4.py
@@ -2,38 +2,31 @@
 import dash
 import dash_daq as daq
 import sys
+import functions as fn
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 from server import app
 from flask import request
 
+
 def show_tab4():
 
     html_tab4 = html.Div([ 
         html.H1(children='Thanks for using impulse, see you back soon!'),
-        html.Button(id='exit-button', children='Click to terminate process'),
+        html.Button(id='exit-button', children=''),
         html.P(children='Always exit the program by clicking the red button, this prevents processes running after browser window is closed.'),
         ],style={'textAlign':'center'}),
 
     return html_tab4
 
-@app.callback(Output(component_id='exit-button', component_property='children'), 
-            Input(component_id='exit-button', component_property='n_clicks'
-        ))
-def prog_exit(n):
+@app.callback(Output('exit-button', 'children'),
+              [Input('exit-button', 'n_clicks')])
 
-    if (n != None): 
-        shutdown()
-        return 'Program closed'
+
+def shutdown_server(n_clicks):
+    if n_clicks is not None:
+        fn.shutdown()
+        return 'Port Closed'
     else:
-        return 'Click to terminate process'
-
-def shutdown():
-    func = request.environ.get('werkzeug.server.shutdown')
-    
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
-
-    
+        return 'Click to Exit'
