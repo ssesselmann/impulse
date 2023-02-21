8b90cf6aff624ce34ce5bdd389d206de4c8f06fa
ssesselmann <75355962+ssesselmann@users.noreply.github.com>
2023-02-21 21:17:17 +1100

    Left tab 3 blank for future counting rate vs time

--- code/tab3.py
+++ code/tab3.py
@@ -8,22 +8,8 @@ from server import app
 def show_tab3():
 
     html_tab3 = html.Div(id='tab3',children=[
-        html.Div(id='tab3_text_div', children=[
-            html.H1('The GS-PRO-V5 Spectrometer'),
-            html.P('This program operates with a sound card spectrometer, this technology was invented in Australia by professor Marek Dolleiser' ),
-            html.P('and the first hardware ever made was the Gammaspectacular GS-1100A back in 2010.' ),
-            html.P('Since then there has been many improvements to the hardware and today we have a highly developed product'), 
-            html.P('working works with a wide range of gamma scintillation detectors and geiger counters.'), 
-            html.P('More information can be found at:'),
-            html.P('https://www.gammaspectacular.com'),
-            html.P('This software is Free open source software.'), 
-            html.P(),
-            html.P('Steven Sesselmann'),
-            html.Div(id='add', children=[html.Img(src='https://www.gammaspectacular.com/steven/impulse/gs_pro_v5.png')]),
-            ]),
 
-        
+        ]),
 
-        ])
     
     return html_tab3
