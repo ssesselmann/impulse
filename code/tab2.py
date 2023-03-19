c86656b9416dd919011b30f21c763219808e1be7
ssesselmann <75355962+ssesselmann@users.noreply.github.com>
2023-03-19 14:08:06 +1100

    Small update to slider

--- code/tab2.py
+++ code/tab2.py
@@ -144,7 +144,7 @@ def show_tab2():
             html.Div(dcc.Input(id='calib_bin_2', type='number', value=calib_bin_2)),
             html.Div(dcc.Input(id='calib_bin_3', type='number', value=calib_bin_3)),
             html.Div('peakfinder'),
-            html.Div(dcc.Slider(id='peakfinder', min=0 ,max=1, step=0.01, value= peakfinder, marks=None,)),
+            html.Div(dcc.Slider(id='peakfinder', min=0 ,max=1, step=0.1, value= peakfinder, marks=['0','1'])),
             ]),
 
         html.Div(id='t2_setting_div', children=[
@@ -153,7 +153,7 @@ def show_tab2():
             html.Div(dcc.Input(id='calib_e_2', type='number', value=calib_e_2)),
             html.Div(dcc.Input(id='calib_e_3', type='number', value=calib_e_3)),
             html.Div('Gaussian corr. (sigma)'),
-            html.Div(dcc.Slider(id='sigma', min=0 ,max=3, step=0.25, value= sigma, marks=['0','1', '2', '3'],)),
+            html.Div(dcc.Slider(id='sigma', min=0 ,max=3, step=0.25, value= sigma, marks=['0','1', '2', '3'])),
             
             ]),
 
