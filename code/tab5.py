2a519868d0be3014f64bde5f616dc3268e8e6d19
Steven Sesselmann <steven@beejewel.com.au>
2024-02-13 12:44:13 +1100

    Try block for no internet

--- code/tab5.py
+++ code/tab5.py
@@ -129,190 +129,198 @@ def update_table_and_page_info(prev_clicks, next_clicks, search_value, current_p
     if not search_value:
         search_value = ''
 
-    # Include current_page in the request
-    response = requests.post('https://gammaspectacular.com/spectra/search', 
-        data={'query': search_value, 'total_pages': total_pages,'current_page': current_page })    
 
-    if response.status_code == 200:
-        response_data   = response.json()
-        spectra_data    = response_data['data']
-        total_pages     = response_data['total_pages']
-        current_page    = response_data['current_page']
+    try:
+        # Include current_page in the request
+        response = requests.post('https://gammaspectacular.com/spectra/search', 
+            data={'query': search_value, 'total_pages': total_pages,'current_page': current_page })    
 
-        # Creating a list of html.Div elements for each record
-        data_divs       = []
-        
+        if response.status_code == 200:
+            response_data   = response.json()
+            spectra_data    = response_data['data']
+            total_pages     = response_data['total_pages']
+            current_page    = response_data['current_page']
 
-        for record in spectra_data:
-            
-            id          = record['id']
-
-            value_list.append(id)
-            
-            filename    = record['filename']
-            date        = record['date']
-            client_info = record['client']
-            spectrum    = record['npes']['data'][0]['resultData']['energySpectrum']['spectrum']
-            location    = record['npes']['data'][0]['sampleInfo']['location']
-            specnote    = record['npes']['data'][0]['sampleInfo']['note']
-            coefficients= record['npes']['data'][0]['resultData']['energySpectrum']['energyCalibration']['coefficients']
-            channels_l  = record['npes']['data'][0]['resultData']['energySpectrum']['numberOfChannels']
-            spec_notes  = record['npes']['data'][0]["sampleInfo"]["note"]
-            
-            channels_s  = len(spectrum)
-            x_factor    = channels_l/channels_s
-            x_values    = [i * x_factor for i in range(channels_s)]
-
-            coeff_1     = coefficients[2] 
-            coeff_2     = coefficients[1]
-            coeff_3     = coefficients[0] + 5 # botch !
-            calib_x     = [coeff_1*x**2 + coeff_2*x + coeff_3 for x in x_values]
-
-            first_name  = client_info.get('first_name', '')
-            last_name   = client_info.get('last_name', '')
-            institution = client_info.get('institution', 'N/A')
-            city        = client_info.get('city', 'N/A')
-            country     = client_info.get('country', 'N/A')
-            email       = client_info.get('email', 'N/A')
-            phone       = client_info.get('phone', 'N/A')
-            web         = client_info.get('web', 'N/A')
-            social      = client_info.get('social', 'N/A')
-            notes       = client_info.get('notes', 'N/A')
-
-            download    = f'https://www.gammaspectacular.com/spectra/files/{id}.json'
-
-            fig         = []
-
-            if institution == None or institution.strip() == "":
-                institution_show = 'none'
-            else: 
-                institution_show = 'block'
-
-            if (city == None or city.strip() == "") and (country == None or country.strip() == ""): 
-                cc_show = 'none'
-            else:
-                cc_show = 'block'
-            
-            if email == None or email.strip() == "": 
-                email_show = 'none'
-            else:
-                email_show = 'block'
-
-            if phone == None or phone.strip() == "": 
-                phone_show = 'none'
-            else:
-                phone_show = 'block'    
-
-            if web == None or web.strip() == "": 
-                web_show = 'none'
-            else:
-                web_show = 'block'
-
-            if social == None or social.strip() == "": 
-                social_show = 'none'
-            else:
-                social_show = 'block'    
-
-            if notes == None or notes.strip() == "": 
-                notes_show = 'none'
-            else:
-                notes_show = 'block' 
-
-            button = html.Button(
-                'zoom', 
-                id={'type': 'zoom-button', 'value': str(id)},
-                n_clicks=0,
-                className='action_button',
-            )
-            buttons.append(button)
-
-            # Format file information
-            file_info_div = html.Div(id='tab5_col_1', children=[
-                html.H4(f"#  {id}"),
-                html.P(f"{date}"),
-                html.P(f"Bins:{channels_l}"),
-                html.A("Download", href=download, target='_blank'),
-            ], style={'textAlign':'left','width': '130px', 'padding': '10px'})
-
+            # Creating a list of html.Div elements for each record
+            data_divs       = []
             
 
-            # Format client information
-            client_info_div = html.Div(id='tab5_col_2', children=[
-                html.P(f"{first_name} {last_name}"),
-                html.P(f"{institution}", style={'display':institution_show}),
-                html.P(f"{city} - {country}", style={'display':cc_show}),
-                html.P(f"e: {email}", style={'fontSize':11, 'display':email_show}),
-                html.P(f"p: {phone}", style={'fontSize':11, 'display':phone_show}),
-                html.P(html.A(web, href=web, target="_blank"), style={'height':10, 'fontSize':10, 'display':web_show}),
-                html.P(html.A(social, href=social, target="_blank"), style={'height':10, 'fontSize':10, 'display':social_show}),
-                html.P(f"Note: {notes}", style={'display':notes_show}),
-                # Include other client details as needed
-            ], style={'width': '250px', 'padding':'10px'})
+            for record in spectra_data:
+                
+                id          = record['id']
 
-            notes_div = html.Div(id='tab5_col_3', children=[
-                html.P(id='spec_notes', children =[spec_notes]),
+                value_list.append(id)
+                
+                filename    = record['filename']
+                date        = record['date']
+                client_info = record['client']
+                spectrum    = record['npes']['data'][0]['resultData']['energySpectrum']['spectrum']
+                location    = record['npes']['data'][0]['sampleInfo']['location']
+                specnote    = record['npes']['data'][0]['sampleInfo']['note']
+                coefficients= record['npes']['data'][0]['resultData']['energySpectrum']['energyCalibration']['coefficients']
+                channels_l  = record['npes']['data'][0]['resultData']['energySpectrum']['numberOfChannels']
+                spec_notes  = record['npes']['data'][0]["sampleInfo"]["note"]
+                
+                channels_s  = len(spectrum)
+                x_factor    = channels_l/channels_s
+                x_values    = [i * x_factor for i in range(channels_s)]
 
-                ], style={'width':'200px', 'height':'175px', 'marginRight':10, 'padding':'5px', 'marginTop':'30px', 'backgroundColor':'#e6f2ff'})
+                coeff_1     = coefficients[2] 
+                coeff_2     = coefficients[1]
+                coeff_3     = coefficients[0] + 5 # botch !
+                calib_x     = [coeff_1*x**2 + coeff_2*x + coeff_3 for x in x_values]
 
+                first_name  = client_info.get('first_name', '')
+                last_name   = client_info.get('last_name', '')
+                institution = client_info.get('institution', 'N/A')
+                city        = client_info.get('city', 'N/A')
+                country     = client_info.get('country', 'N/A')
+                email       = client_info.get('email', 'N/A')
+                phone       = client_info.get('phone', 'N/A')
+                web         = client_info.get('web', 'N/A')
+                social      = client_info.get('social', 'N/A')
+                notes       = client_info.get('notes', 'N/A')
 
-            plot_div = html.Div(id='tab5_col_4', children=[dcc.Graph(figure=fig)]),
+                download    = f'https://www.gammaspectacular.com/spectra/files/{id}.json'
 
-            button_div = html.Div(id='tab5_col_5', children=[button], style={
-                'width': '70px',  # Ensure units are specified for width and height
-                'height': '200px',
-                'display': 'flex',  # Make the div a flex container
-                'flex-direction': 'column',  # Stack children vertically
-                'justify-content': 'flex-end',  # Align children to the end (bottom) of the container
-                })
+                fig         = []
 
+                if institution == None or institution.strip() == "":
+                    institution_show = 'none'
+                else: 
+                    institution_show = 'block'
 
-            # Generate a basic plot
-            fig = go.Figure(data=[go.Scatter(
-                x = calib_x,
-                y=spectrum, 
-                mode='lines',
-                fill='tozeroy',
-                line=dict(color='black'),
-                fillcolor='black'
-            )])
-            fig.update_layout(
-                title=filename,
-                title_font_size=14,
-                height=225,
-                width=400,
-                plot_bgcolor='#e6f2ff',
-                margin=dict(l=5, r=5, t=30, b=5), 
-                xaxis={'visible': True}, 
-                yaxis={'visible': False}
-            )
+                if (city == None or city.strip() == "") and (country == None or country.strip() == ""): 
+                    cc_show = 'none'
+                else:
+                    cc_show = 'block'
+                
+                if email == None or email.strip() == "": 
+                    email_show = 'none'
+                else:
+                    email_show = 'block'
 
-            # Plot div with the graph
-            plot_div = html.Div([
-                dcc.Graph(
-                    figure=fig,
-                    style={'height': '100%', 'width': '100%'}
+                if phone == None or phone.strip() == "": 
+                    phone_show = 'none'
+                else:
+                    phone_show = 'block'    
+
+                if web == None or web.strip() == "": 
+                    web_show = 'none'
+                else:
+                    web_show = 'block'
+
+                if social == None or social.strip() == "": 
+                    social_show = 'none'
+                else:
+                    social_show = 'block'    
+
+                if notes == None or notes.strip() == "": 
+                    notes_show = 'none'
+                else:
+                    notes_show = 'block' 
+
+                button = html.Button(
+                    'zoom', 
+                    id={'type': 'zoom-button', 'value': str(id)},
+                    n_clicks=0,
+                    className='action_button',
                 )
-            ])
+                buttons.append(button)
 
-            i = i+1
+                # Format file information
+                file_info_div = html.Div(id='tab5_col_1', children=[
+                    html.H4(f"#  {id}"),
+                    html.P(f"{date}"),
+                    html.P(f"Bins:{channels_l}"),
+                    html.A("Download", href=download, target='_blank'),
+                ], style={'textAlign':'left','width': '130px', 'padding': '10px'})
 
-            # Combine the divs into a single row
-            row_div = html.Div([file_info_div, client_info_div, notes_div, plot_div, button_div], 
-                style={
-                    'display':'flex',
-                    'flex-wrap':'wrap',
-                    'align-items': 'flex-start',  
-                    'justify-content': 'center',  
-                    'width':'100%',
-                    'clear': 'both',
+                
+
+                # Format client information
+                client_info_div = html.Div(id='tab5_col_2', children=[
+                    html.P(f"{first_name} {last_name}"),
+                    html.P(f"{institution}", style={'display':institution_show}),
+                    html.P(f"{city} - {country}", style={'display':cc_show}),
+                    html.P(f"e: {email}", style={'fontSize':11, 'display':email_show}),
+                    html.P(f"p: {phone}", style={'fontSize':11, 'display':phone_show}),
+                    html.P(html.A(web, href=web, target="_blank"), style={'height':10, 'fontSize':10, 'display':web_show}),
+                    html.P(html.A(social, href=social, target="_blank"), style={'height':10, 'fontSize':10, 'display':social_show}),
+                    html.P(f"Note: {notes}", style={'display':notes_show}),
+                    # Include other client details as needed
+                ], style={'width': '250px', 'padding':'10px'})
+
+                notes_div = html.Div(id='tab5_col_3', children=[
+                    html.P(id='spec_notes', children =[spec_notes]),
+
+                    ], style={'width':'200px', 'height':'175px', 'marginRight':10, 'padding':'5px', 'marginTop':'30px', 'backgroundColor':'#e6f2ff'})
+
+
+                plot_div = html.Div(id='tab5_col_4', children=[dcc.Graph(figure=fig)]),
+
+                button_div = html.Div(id='tab5_col_5', children=[button], style={
+                    'width': '70px',  # Ensure units are specified for width and height
+                    'height': '200px',
+                    'display': 'flex',  # Make the div a flex container
+                    'flex-direction': 'column',  # Stack children vertically
+                    'justify-content': 'flex-end',  # Align children to the end (bottom) of the container
                     })
 
-            data_divs.append(row_div)
 
-        new_page_info = f"Page {current_page} of {total_pages}"
+                # Generate a basic plot
+                fig = go.Figure(data=[go.Scatter(
+                    x = calib_x,
+                    y=spectrum, 
+                    mode='lines',
+                    fill='tozeroy',
+                    line=dict(color='black'),
+                    fillcolor='black'
+                )])
+                fig.update_layout(
+                    title=filename,
+                    title_font_size=14,
+                    height=225,
+                    width=400,
+                    plot_bgcolor='#e6f2ff',
+                    margin=dict(l=5, r=5, t=30, b=5), 
+                    xaxis={'visible': True}, 
+                    yaxis={'visible': False}
+                )
 
-        
-        return data_divs, total_pages, current_page, new_page_info
+                # Plot div with the graph
+                plot_div = html.Div([
+                    dcc.Graph(
+                        figure=fig,
+                        style={'height': '100%', 'width': '100%'}
+                    )
+                ])
+
+                i = i+1
+
+                # Combine the divs into a single row
+                row_div = html.Div([file_info_div, client_info_div, notes_div, plot_div, button_div], 
+                    style={
+                        'display':'flex',
+                        'flex-wrap':'wrap',
+                        'align-items': 'flex-start',  
+                        'justify-content': 'center',  
+                        'width':'100%',
+                        'clear': 'both',
+                        })
+
+                data_divs.append(row_div)
+
+            new_page_info = f"Page {current_page} of {total_pages}"
+
+            
+            return data_divs, total_pages, current_page, new_page_info
+
+    except:
+        # Handle the case where there's no internet connection
+        print("No internet connection available.")
+        new_page_info = 'No internet connection - unable to update'
+        return [], '0', '1', new_page_info
 
     else:
 
