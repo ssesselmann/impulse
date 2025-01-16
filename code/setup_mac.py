cac4d547f952c19b109284eaa6ec51cc070fef82
ssesselmann <75355962+ssesselmann@users.noreply.github.com>
2025-01-16 20:21:47 +1100

    version

--- code/setup_mac.py
+++ code/setup_mac.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='impulse',
-    version='2.2.5',
+    version='2.2.7',
     packages=find_packages(),
     install_requires=[
         'dash',
